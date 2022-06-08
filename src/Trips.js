1f925098243a1be28f7d13a0da81853e8d331826
Amanda Sands <amandajean555@gmail.com>
2022-06-08 08:51:36 -0600

    Move organizational comments for better visibility

--- src/Trips.js
+++ src/Trips.js
@@ -1,6 +1,6 @@
+    //gets all trips for one user
 class Trips{
   constructor(travelerId, allTripsData){
-    //gets all trips for one user
     this.travelerId = travelerId;
     this.allTrips = allTripsData;
   }
