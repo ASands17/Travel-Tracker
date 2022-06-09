1006acf49928b7d63db9e43dbbe38a58936700ef
Amanda Sands <amandajean555@gmail.com>
2022-06-09 11:36:58 -0600

    Remove notes from Traveler.js file

--- src/Traveler.js
+++ src/Traveler.js
@@ -2,8 +2,6 @@ class Traveler {
   constructor(singleTraveler) {
     this.id = singleTraveler.id;
     this.name = singleTraveler.name;
-    //may add traveler type later to add flair to greeting
-    // this.travelerType = singleTraveler.travelerType;
   }
 
   getTravelerName() {
@@ -11,13 +9,4 @@ class Traveler {
   }
 }
 
-
-
 export default Traveler;
-
-//
-// Your testing suite should test all of the functionality of the application, including the following:
-//
-// Class default properties
-// Class methods
-// Anything that updates class properties
