53b868226bf1c75e5c4efffbd22cd290ce17fe35
Amanda Sands <amandajean555@gmail.com>
2022-06-12 10:49:35 -0600

    Add initial query selectors

--- src/scripts.js
+++ src/scripts.js
@@ -1,7 +1,6 @@
 // This is the JavaScript entry file - your code begins here
 // Do not delete or rename this file ********
 
-import {getTravelers, getTrips, getDestinations} from './API-fetch';
 
 // An example of how you tell webpack to use a CSS (SCSS) file
 // import './css/styles.css';
@@ -12,12 +11,39 @@ import {getTravelers, getTrips, getDestinations} from './API-fetch';
 
 // console.log('This is the JavaScript entry file - your code begins here.');
 
+//IMPORTS
+import {getTravelers, getTrips, getDestinations} from './API-fetch';
+import './css/styles.css';
 
 //GLOBAL VARIABLES
 var globalTravelers, globalTrips, globalDestinations;
 
+//QUERY SELECTORS
+
+var nameDisplay = document.querySelector('#nameGreeting')
+
+var presentDestination = document.querySelector('#presentTripsDestination')
+var presentDates = document.querySelector('#presentTripsDates')
+var presentTravelers = document.querySelector('#presentTripsTravelers')
+
+var upcomingDestination = document.querySelector('#upcomingTripsDestination')
+var upcomingDates = document.querySelector('#upcomingTripsDates')
+var upcomingTravelers = document.querySelector('#upcomingTripsTravelers')
+
+var pastDestination = document.querySelector('#pastTripsDestination')
+var pastDates = document.querySelector('#pastTripsDates')
+var pastTravelers = document.querySelector('#pastTripsTravelers')
+
+var pendingDestination = document.querySelector('#pendingTripsDestination')
+var pendingDates = document.querySelector('#pendingTripsDates')
+var pendingTravelers = document.querySelector('#pendingTripsTravelers')
+
+var requestTripButton = document.querySelector('#requestTripsDepartureButton')
+
+
 //EVENT LISTENERS
 window.addEventListener('load', getGlobalDataFromAPI);
+
 
 //EVENT HANDLERS
 
@@ -31,10 +57,23 @@ function getGlobalDataFromAPI() {
     globalTrips = values[1];
     globalDestinations = values[2];
     startFnOffHere();
+
     //Start DOM manip from fn above (will rename)
   })
 }
 
+//FUNCTIONS
 function startFnOffHere() {
   console.log("globalData in method", globalTravelers, globalTrips, globalDestinations);
+  showTraveler(globalTravelers);
+}
+
+function getRandomTraveler(allTravelers) {
+  return allTravelers.travelers[Math.floor(Math.random()*allTravelers.travelers.length)];
+}
+
+function showTraveler(globalTravelers) {
+  let randomTraveler = getRandomTraveler(globalTravelers);
+  console.log("random", randomTraveler)
+  nameDisplay.innerHTML += randomTraveler.name;
 }
